03f892caac28a51a44998873acfd93724b8fa990
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-22 17:16:36 +0100

    Test register function from use user custom hook

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,21 +2,22 @@ import { rest } from "msw";
 import { REACT_APP_API_SKYBALL } from "@env";
 import { type UserRegisterCredentials } from "../types";
 
-const url = REACT_APP_API_SKYBALL;
-
 const handlers = [
-  rest.post(`${url}/users/register`, async (req, res, ctx) => {
-    const { email } = await req.json<UserRegisterCredentials>();
+  rest.post(
+    `${REACT_APP_API_SKYBALL}/users/register`,
+    async (req, res, ctx) => {
+      const user = await req.json<UserRegisterCredentials>();
 
-    if (!email) {
-      return res(
-        ctx.status(409),
-        ctx.json({ error: "User is already registered" })
-      );
+      if (!user || user.email === "mock@gmail.com") {
+        return res(
+          ctx.status(409),
+          ctx.json({ error: "User successfully registered" })
+        );
+      }
+
+      return res(ctx.status(201), ctx.json({ user }));
     }
-
-    return res(ctx.status(201), ctx.json({}));
-  }),
+  ),
 ];
 
 export default handlers;
---
+++ src/mocks/userMocks.ts
@@ -0,0 +1,9 @@
+import { type UserRegisterCredentials } from "../types";
+
+export const registerDataMock: UserRegisterCredentials = {
+  password: "1234",
+  email: "mireia@gmail.com",
+  gender: "f",
+  level: 2,
+  name: "Mireia",
+};
