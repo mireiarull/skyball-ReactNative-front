acf5a7320fd523832c151456674ba08a6df7f54f
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-24 22:59:53 +0100

    Add welcome screen styles

---
+++ src/screens/WelcomeScreen/WelcomeScreenStyles.tsx
@@ -0,0 +1,32 @@
+import { Dimensions, StyleSheet } from "react-native";
+import buttonStyles from "../../styles/buttonStyles";
+import colorStyles from "../../styles/colorStyles";
+
+const welcomeScreenStyles = StyleSheet.create({
+  container: {
+    display: "flex",
+    height: Dimensions.get("window").height,
+    padding: 25,
+    justifyContent: "flex-end",
+  },
+  button: {
+    ...buttonStyles.button,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  image: {
+    height: Dimensions.get("window").height,
+    width: Dimensions.get("window").width,
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  link: {
+    textAlign: "center",
+    fontWeight: "600",
+    fontSize: 20,
+    color: colorStyles.white,
+  },
+});
+
+export default welcomeScreenStyles;
