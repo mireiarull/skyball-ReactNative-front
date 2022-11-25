5fa5649795e7e4165b24072d4b7f586143f1ce5a
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-25 10:43:40 +0100

    Fix loading component test with preloaded state true

--- src/components/Loading/Loading.test.tsx
+++ src/components/Loading/Loading.test.tsx
@@ -1,20 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from "react";
-import { render, screen } from "@testing-library/react-native";
-import { Provider } from "react-redux";
-import { store } from "../../redux/store";
+import { screen } from "@testing-library/react-native";
 import Loading from "./Loading";
+import { renderWithProviders } from "../../test-utils/renderWithProviders";
+import { mockInitialUiState, mockInitialUserState } from "../../mocks/uiMocks";
 
 describe("Given a Loading component", () => {
   describe("When it's rendered", () => {
     test("Then it should show the loading gif", async () => {
       const loadingId = "loading";
 
-      render(
-        <Provider store={store}>
-          <Loading />
-        </Provider>
-      );
+      renderWithProviders(<Loading />, {
+        preloadedState: {
+          ui: mockInitialUiState,
+          user: mockInitialUserState,
+        },
+      });
 
       const loading = screen.getByTestId(loadingId);
 
--- src/mocks/uiMocks.ts
+++ src/mocks/uiMocks.ts
@@ -1,14 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { type UiState } from "../redux/features/uiSlice/types";
 import { uiReducer } from "../redux/features/uiSlice/uiSlice";
 import { type UserState } from "../redux/features/userSlice/types";
 import { userReducer } from "../redux/features/userSlice/userSlice";
 import { type store } from "../redux/store";
 
-export const mockInitialUiState = {
+export const mockInitialUiState: UiState = {
   showModal: true,
   isError: false,
   modalText: "Success",
   buttonText: "Continue",
+  isLoading: true,
+  modalTitle: "",
 };
 
 export const mockInitialUserState: UserState = {
