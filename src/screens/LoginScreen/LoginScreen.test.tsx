34667578e393b84a8631ed2a2f1ee00a534aea82
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-26 10:10:50 +0100

    Add navigation test to login screen

--- src/screens/LoginScreen/LoginScreen.test.tsx
+++ src/screens/LoginScreen/LoginScreen.test.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from "react";
-import { render, screen } from "@testing-library/react-native";
+import { render, screen, fireEvent } from "@testing-library/react-native";
 
 import { Provider } from "react-redux";
 import { store } from "../../redux/store";
@@ -12,6 +13,18 @@ const mockLoginUser = jest.fn();
 jest.mock("../../hooks/useUser/useUser", () => () => ({
   loginUser: mockLoginUser,
 }));
+
+const mockedGoBack = jest.fn();
+
+jest.mock("@react-navigation/native", () => {
+  const actualNav = jest.requireActual("@react-navigation/native");
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      goBack: mockedGoBack,
+    }),
+  };
+});
 
 describe("Given a LoginScreen page", () => {
   describe("When it's rendered", () => {
@@ -29,6 +42,25 @@ describe("Given a LoginScreen page", () => {
       const displayedImage = screen.queryByTestId(imageId);
 
       expect(displayedImage).toBeDefined();
+    });
+  });
+
+  describe("When the user clicks the back arrow button", () => {
+    test("Then the useNavigation should be called with the goBack method", async () => {
+      const backArrowId = "backArrow";
+
+      render(
+        <Provider store={store}>
+          <NavigationContainer>
+            <LoginScreen />
+          </NavigationContainer>
+        </Provider>
+      );
+
+      const goBackButton = await screen.getByTestId(backArrowId);
+      fireEvent(goBackButton, "press");
+
+      expect(mockedGoBack).toHaveBeenCalled();
     });
   });
 });
